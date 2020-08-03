566bfb52643bca08b1db4793a52fb2615b82aa3c
paul martin <mussym@live.com>
2020-08-02 19:08:21 -0500

    going with fxml in main window

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,10 +1,12 @@
 import com.googlecode.lanterna.{SGR, TerminalPosition, TextColor}
 import com.googlecode.lanterna.terminal.{DefaultTerminalFactory, Terminal}
 import javafx.application.Application
-import javafx.scene.Scene
+import javafx.scene.{Parent, Scene}
 import javafx.scene.control.Label
 import javafx.scene.layout.StackPane
 import javafx.stage.Stage
+import javafx.fxml.FXMLLoader
+
 
 object Main {
 
@@ -23,11 +25,14 @@ object Main {
 class HelloFX extends Application {
 
   override def start(primaryStage: Stage): Unit = {
+    val root: Parent = FXMLLoader.load(getClass().getResource("sample.fxml"))
     val javaVersion = System.getProperty("java.version")
     val javafxVersion = System.getProperty("javafx.version")
-    val l = new Label(s"hello javafx ${javafxVersion} , running on java ${javaVersion}")
-    val scene = new Scene(new StackPane(l), 640, 480)
-    primaryStage.setScene(scene)
+    primaryStage.setTitle("Dotty on javafx")
+    primaryStage.setScene(new Scene(root, 300, 275))
+    //val l = new Label(s"hello javafx ${javafxVersion} , running on java ${javaVersion}")
+    //val scene = new Scene(new StackPane(l), 640, 480)
+    //primaryStage.setScene(scene)
     primaryStage.show()
   }
 
